94d38433a53e07031f07bf10cc42728cd0af591b
Léo <leo.legron@gmail.com>
2019-02-21 17:59:25 +0100

    New CI Configuration

--- CI/before_deploy.sh
+++ CI/before_deploy.sh
@@ -6,9 +6,13 @@ then
     git tag $TRAVIS_TAG 
     cd .build/x86_64-apple-macosx10.10/release 
     tar czvf ilogyou.tar.gz iLogYou
-else
+    cd ../../..
     git clone https://github.com/leolgrn/homebrew-ilogyou.git 
-    cd homebrew-ilogyou/Formula 
+    git remote remove origin
+    git remote add origin https://leolgrn:${GH_TOKEN}github.com/leolgrn/homebrew-ilogyou.git
     touch test.swift 
+    git add .
+    git commit -m "Testing pushing file from Travis"
+    git push origin master --quiet
     cd ..
 fi
