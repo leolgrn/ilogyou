96f9b3731bf39fa38c8142a6f337e7833079f346
Léo <leo.legron@gmail.com>
2019-02-21 18:10:12 +0100

    Update before_deploy

--- CI/before_deploy.sh
+++ CI/before_deploy.sh
@@ -6,10 +6,11 @@ then
     git tag $TRAVIS_TAG 
     cd .build/x86_64-apple-macosx10.10/release 
     tar czvf ilogyou.tar.gz iLogYou
-    cd ../../..
-    git clone https://github.com/leolgrn/homebrew-ilogyou.git 
+    cd ../../../..
+    git clone https://github.com/leolgrn/homebrew-ilogyou.git
+    cd homebrew-ilogyou
     git remote remove origin
-    git remote add origin https://leolgrn:${GH_TOKEN}github.com/leolgrn/homebrew-ilogyou.git
+    git remote add origin https://leolgrn:${GH_TOKEN}@github.com/leolgrn/homebrew-ilogyou.git
     touch test.swift 
     git add .
     git commit -m "Testing pushing file from Travis"
