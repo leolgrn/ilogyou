d5b61048fb5d86c851b58412915478c4dda4d306
Léo <leo.legron@gmail.com>
2019-02-22 11:34:07 +0100

    Automated formula updates

--- CI/before_deploy.sh
+++ CI/before_deploy.sh
@@ -6,14 +6,21 @@ then
     git tag $TRAVIS_TAG 
     cd .build/x86_64-apple-macosx10.10/release 
     tar czvf ilogyou.tar.gz iLogYou
+    hash=`shasum -a 256 ilogyou.tar.gz | awk '{printf $1}'`
+    hashLine='  sha256 "'$hash'"'
+    url='  url "https://github.com/leolgrn/ilogyou/releases/download/'$TRAVIS_TAG'/ilogyou.tar.gz"'
     cd ../../../..
     git clone https://github.com/leolgrn/homebrew-ilogyou.git
     cd homebrew-ilogyou
     git remote remove origin
     git remote add origin https://leolgrn:${GH_TOKEN}@github.com/leolgrn/homebrew-ilogyou.git
-    touch test.swift 
+    awk -v url="$url" 'NR==4 { $0=url } { print $0 }' Formula/ilogyou.rb > Formula/ilogyou2.rb
+    awk -v hashLine="$hashLine" 'NR==6 { $0=hashLine } { print $0 }' Formula/ilogyou2.rb > Formula/ilogyou3.rb
+    rm Formula/ilogyou.rb
+    rm Formula/ilogyou2.rb
+    mv Formula/ilogyou3.rb Formula/ilogyou.rb
     git add .
-    git commit -m "Testing pushing file from Travis"
+    git commit -m "Update Formula"
     git push origin master --quiet
-    cd ..
+    cd ../ilogyou
 fi
